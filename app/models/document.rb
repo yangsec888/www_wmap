ccc1b73d6de374752d18cdee7353a9de717c8fa7
Yang Li <ylee@Yangs-MacBook-Pro.local>
2018-01-19 11:17:01 -0500

    disable carriwave uploader

--- app/models/document.rb
+++ app/models/document.rb
@@ -8,7 +8,7 @@
 #++
 
 class Document < ActiveRecord::Base
-  mount_uploader :attachment, AttachmentUploader
-	validates :name, presence: true
+  #mount_uploader :attachment, AttachmentUploader
+	#validates :name, presence: true
 
 end
