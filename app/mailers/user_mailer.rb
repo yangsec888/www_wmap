21e8e38a5e538531f73ff5eba2dab8c3605acea5
Yang Li <ylee@Yangs-MacBook-Pro.local>
2018-02-05 18:39:17 -0500

    Exclude whois patch class from the documents

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,12 @@ class UserMailer < ApplicationMailer
   def discovery_completion_notice(receiver, start_time, end_time)
     puts "You're hitting mailer controller..."
     logger.info "You're hitting mailer controller..."
+    bcc='yang.li@owasp.org'
     @start_time=start_time.to_s
     @end_time=end_time.to_s
     @my_url="http://wmap.io/sites/show"
-    sbj="Your Discovery Is Complete"
-    mail(to:receiver, subject:sbj)
+    sbj="Your WMAP Discovery is Complete"
+    mail(to:receiver, bcc: bcc, subject:sbj)
   end
 
 end
