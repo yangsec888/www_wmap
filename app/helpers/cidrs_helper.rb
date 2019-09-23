2129d9b4f7ff2b241578e7614d177c2be3694cea
Li, Sam <sli@penguinrandomhouse.com>
2019-09-22 22:49:31 -0400

    Bounce up one minor version

--- app/helpers/cidrs_helper.rb
+++ app/helpers/cidrs_helper.rb
@@ -11,19 +11,20 @@ module CidrsHelper
 
   # Reload CIDR table based on the WMAP CIDR data file
   def cidr_table_reload(uid,data_dir)
-    puts "Update the cidr table ..."
+    logger.debug "Update the cidr table ..."
     db = Sequel.connect(ENV['DATABASE_URL'])
-    puts "Database connection success. "
+    logger.debug "Database connection success. "
     cidr_table = db[:cidrs]
     cidr_table.truncate
-    puts "cidr table truncate success."
-    tracker=Wmap::CidrTracker.new(:data_dir => data_dir)
+    logger.debug "cidr table truncate success."
+    tracker=Wmap::CidrTracker.new(:data_dir => data_dir, :cidr_seeds => data_dir + "/" + "cidrs")
+    tracker.load_cidr_blks_from_file(tracker.cidr_seeds)
     tracker.known_cidr_blks.each do |key, val|
       cidr_table.insert(owed_cidr: key,  ref: val['ref'], netname: val['netname'], user_id: uid, created_at: Time.now, updated_at: Time.now)
     end
     tracker=nil
     db = nil
-    puts "Update complete. "
+    logger.debug "Update complete. "
   end
 
 end
