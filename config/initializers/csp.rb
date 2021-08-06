ead2059aca068daf08734ca2d232feb825456009
Sam (Yang) Li <sli@MAC-sli.local>
2021-08-06 15:39:48 -0400

    Add link to site_url index page; update csp add gem requirement

--- config/initializers/csp.rb
+++ config/initializers/csp.rb
@@ -1,8 +1,10 @@
+require 'secure_headers'
+
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('none'), # nothing allowed by default
     script_src: %w('self' 'unsafe-inline'),
-    worker_src: %w('self' blob:),   # allow js tree to spawn workers 
+    worker_src: %w('self' blob:),   # allow js tree to spawn workers
     connect_src: %w('self'),
     img_src: %w('self' data:),
     font_src: %w('self' data:),
