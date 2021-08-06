0f0d88946acc79c2d2156df4436fc3106a9a0004
Sam (Yang) Li <sli@MAC-sli.local>
2021-08-06 17:54:12 -0400

    Bug fix - host_table_reload function

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -19,13 +19,18 @@ module HostsHelper
     tracker=Wmap::HostTracker.instance
     tracker.data_dir = data_dir
     tracker.hosts_file = tracker.data_dir + "/" + "hosts"
-    tracker.load_known_hosts_from_file(tracker.hosts_file)
-    tracker.known_hosts.each do |key, val|
+    known_hosts=tracker.load_known_hosts_from_file(tracker.hosts_file)
+    known_hosts.each do |key, val|
       block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
       re = /\A#{block}\.#{block}\.#{block}\.#{block}\z/
-      next if re =~ key
-      host_table.insert(host_name: key,  ip: val, \
-        uid: uid, created_at: Time.now, updated_at: Time.now)
+      if re =~ key
+        #puts "Skip host: #{key}"
+        next
+      else
+        puts "Insert record #{key} => #{val} to host table ..."
+        host_table.insert(host_name: key,  ip: val, \
+          uid: uid, created_at: Time.now, updated_at: Time.now)
+      end
     end
     tracker=nil
     db = nil
